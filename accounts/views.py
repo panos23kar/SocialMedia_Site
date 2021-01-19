42c9d9a727b713d3cf085e707d1bd97caa54a45c
panos23kar <panos23kar@gmail.com>
2021-01-19 18:18:30 +0100

    fixed typo in accounts.views

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,6 @@ from . import forms
 # Create your views here.
 
 class SignUp(CreateView):
-    form_class = form.UserCreateForm
+    form_class = forms.UserCreateForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
