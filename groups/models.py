418e06e3e83c9e76bd5a6c6a40f10aa2e170a4eb
panos23kar <panos23kar@gmail.com>
2021-01-22 19:18:47 +0100

    import to register custom template tags

--- groups/models.py
+++ groups/models.py
@@ -1,8 +1,14 @@
 from django.db import models.sl
 from django.utils.text import slugify
 import misaka
-from django.contrib.auth import get_user_model
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django import template
+register = template.Library()
 # Create your models here.
+
+
 
 
