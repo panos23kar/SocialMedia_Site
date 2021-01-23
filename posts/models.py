5ab76b60f34703846e0bfa3a8448cc6c3e2d77da
panos23kar <panos23kar@gmail.com>
2021-01-23 20:30:47 +0100

    import all essential libs for models of posts app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+from django.conf import settings
 
-# Create your models here.
+import misaka
+
+from groups.models import Group
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
