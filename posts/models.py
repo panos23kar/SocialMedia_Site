22f4032afe61c7a9b1015857becbcf404d63393b
panos23kar <panos23kar@gmail.com>
2021-01-23 20:36:21 +0100

    create Post model in posts app models

--- posts/models.py
+++ posts/models.py
@@ -8,3 +8,10 @@ from groups.models import Group
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
+
+class Post(models.Model):
+    user = models.ForeignKey(User, related_name='posts')
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(Group, related_name='posts', null=True, blank=True)
