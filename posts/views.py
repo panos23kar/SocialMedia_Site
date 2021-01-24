529dc676dba94ac8f9c26d25f80824d10e6123d8
panos23kar <panos23kar@gmail.com>
2021-01-24 13:45:37 +0100

    imported required libs for views of posts app

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
+from django.http import Http404
+from django.views import generic
+
+from braces.views import SelectRelatedMixin
+
+from . import models
+from . import forms
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
