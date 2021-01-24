6e7126d17f9b673c62bbd9ba78eca1f3cf384139
panos23kar <panos23kar@gmail.com>
2021-01-24 20:02:25 +0100

    create view for goups app

--- groups/views.py
+++ groups/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         PermissionRequiredMixin)
 
 from django.core.urlresolvers import reverse
 from django.views import generic
-
+from django.shortcuts import get_object_or_404
 from groups.models import Group, GroupMember
 
 class CreateGroup(LoginRequiredMixin, generic.CreateView):
@@ -16,3 +17,20 @@ class SingleGroup(generic.DetailView):
 
 class ListGroups(generic.ListView):
     model = Group
+
+class Joingroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug':self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+
+        try:
+            GroupMember.objects.create(user=self.request.user,group=group)
+        except IntegrityError:
+            messages.warning(self.request, 'Warning already a member')
+        else:
+            messages.success(self.request, 'You are now a member')
+        
+        return super().get(request,*args, **kwargs)
