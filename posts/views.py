03f365d04d239b028550d22ffd67f90390cb4d92
panos23kar <panos23kar@gmail.com>
2021-01-24 14:10:28 +0100

    create UserPosts and get_queryset method

--- posts/views.py
+++ posts/views.py
@@ -16,3 +16,17 @@ User = get_user_model()
 class PostList(SelectRelatedMixin, generic.ListView):
     model = models.Post
     select_related = ('user', 'group')
+
+
+class UserPosts(generic.ListView):
+    model = models.Post
+    template_name = 'posts/user_post_list.html'
+
+    def get_queryset(self):
+        try:
+            self.post.user = User.objects.prefetch_related('posts').get(username__iexact= self.kwargs.get("username"))
+        except User.DoesNotExist:
+            raise Http404
+        else:
+            return self.post_user.posts.all()
+        
