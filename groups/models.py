1cb1beda012f2e2f65257a180fcaa00a45947dd7
panos23kar <panos23kar@gmail.com>
2021-01-24 21:47:35 +0100

    from django.urls import reverse in groups models

--- groups/models.py
+++ groups/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 import misaka
+from django.urls import reverse
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
