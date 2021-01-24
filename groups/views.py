4a010fbc256dd3133e9ea5cb9abcd33132074b39
panos23kar <panos23kar@gmail.com>
2021-01-24 20:14:04 +0100

    create LeaveGroup view for groups app

--- groups/views.py
+++ groups/views.py
@@ -34,3 +34,22 @@ class Joingroup(LoginRequiredMixin, generic.RedirectView):
             messages.success(self.request, 'You are now a member')
         
         return super().get(request,*args, **kwargs)
+    
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug':self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+
+        try:
+            membership = models.GroupMember.objects.filter(
+                user=self.request.user,
+                group__slug=self.kwargs.get('slug')
+            ).get()
+        except models.GroupMember.DoesNotExist:
+            messages.warning(self.request, 'Sorry you are not in Group!')
+        else:
+            membership.delete()
+            messages.success(self.request, 'You have left the group!')
+        return super().get(request, *args, **kwargs)
